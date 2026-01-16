60cccb5a6c8fd932f34a3167b812288affa3c580
Joyce38 <joycedjoko38@gmail.com>
2026-01-16 13:39:05 +0100

    added the state design pattern

---
+++ state/state.php
@@ -0,0 +1,11 @@
+<?php
+// State.php
+declare(strict_types=1);
+
+namespace DesignPatterns\Behavioral\State;
+
+interface State
+{
+    public function proceedToNext(OrderContext $context);
+    public function toString(): string;
+}
